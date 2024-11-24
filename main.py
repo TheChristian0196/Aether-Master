cbe7cd45e402a6c4bf4939bb44201adf58360249
leo26k <94547103+leo26k@users.noreply.github.com>
2024-11-24 19:59:18 +0000

    added flag emojis

--- main.py
+++ main.py
@@ -134,7 +134,7 @@ async def stats(ctx):
 	database=read_db()
 
 	# make a stats message
-	final_msg=f"**{player}**\n\n"
+	final_msg=f"**{player}**     <:{config['player_flags'][player]['emoji_name']}:{config['player_flags'][player]['emoji_id']}> \n\n"
 	gold=database[player]['gold']
 	food=database[player]['food']
 	pop=database[player]['pop']
@@ -200,7 +200,7 @@ async def new_turn(ctx, turn):
 	for player in database:
 		gold = database[player]['gold']
 		food = database[player]['food']
-		finnal_txt += f"**{player}** \n**Gold:** {gold}\n**Food:** {food}\n"
+		finnal_txt += f"**{player}**     <:{config['player_flags'][player]['emoji_name']}:{config['player_flags'][player]['emoji_id']}>\n**Gold:** {gold}\n**Food:** {food}\n"
 		n = 1
 		
 		orders_txt = "**Orders:** \n"
